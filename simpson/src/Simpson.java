4495dfdac41f4d9142c34b52f456c0453612aa5d
JimenezAndrewJ <andrewjairjh@gmail.com>
2024-10-04 12:44:02 +0200

    [04/10/2024 12:43 cambios en el codigo simpson]
    [04/10/2024 12:43 cambios en el codigo simpson]

--- simpson/src/Simpson.java
+++ simpson/src/Simpson.java
@@ -2,23 +2,23 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Simpson {
     // VISIBLE A NIVEL DE LA CLASE SMPSON
-    static char[][] tablero=  new char[10][10];
-    char personaje = '@';
+    static char[][] tablero=  new char[10][10];//crear matriz
+    char personaje = '@';//darle valor a personaje
     //FIN
 
     /*software de gestión de venta ONLINE*/
 
-    public void rellenarTablero(){
+    public void rellenarTablero(){//publico no estatico
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                tablero[i][j] = personaje;
+                tablero[i][j] = personaje;//rellenar tablero
             }
         }
     }
 
     public static void main(String[] args) {
         Simpson simpson = new Simpson();
-        simpson.rellenarTablero();
+        simpson.rellenarTablero(); //llamo al algoritmo para el tablero
         System.out.println("tablero:");
         //mostrar tablero
         for (int i = 0; i < 10; i++) {
