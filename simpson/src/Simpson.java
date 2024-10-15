3f9e14997b32b88b2450d2c8de3d1f35e0012af8
JimenezAndrewJ <andrewjairjh@gmail.com>
2024-10-15 11:50:52 +0200

    [15/10/2024 11:49 VIDEO 2 DE EJERCICIO DE SIMPSON]
    [15/10/2024 11:49 VIDEO 2 DE EJERCICIO DE SIMPSON]

--- simpson/src/Simpson.java
+++ simpson/src/Simpson.java
@@ -20,57 +20,68 @@ public class Simpson {
             System.out.println();
         }
     }
-    public static void main(String[] args) {
-
-        tablero=new char[MAX_FILA_T][MAX_COLUMNA_T];//traemos el tablero dentro de este void
-        //LLENAR TABLERO 'L'
+    private static void AsignarCaracterACasillasLibres(char caracter) {
         for (int i = 0; i < MAX_FILA_T; i++) {//Primero se selecciona la fila
             for (int j = 0; j < MAX_COLUMNA_T; j++) {//luego se llena la columna entera y cambia de fila
-                    tablero[i][j] = 'L';//ponemos el valor dentro de las coordenadas [i] y [j]
+                tablero[i][j] = caracter;//ponemos el valor dentro de las coordenadas [i] y [j]
 
             }
         }
-        //mostrar tablero arriba esta definido
-       printtablero();
-
-                //PARTE DE BART
-
-            Random aleatorio = new Random();//traemos la funcion Random de la libreria de java
+    }
+    private static void AsignarBartACasillaLibre(char caracter,int numRepeticiones){
+        Random aleatorio = new Random();//traemos la funcion Random de la libreria de java
         //esto nos dara 2 valores de fila y columna para usarlo como una coordenada de una posicion aleatoria
-            int filaAleatorio = aleatorio.nextInt(MAX_FILA_T);//ira del 0-9 por que la matriz es de 10 valores
-            int columnaAleatorio = aleatorio.nextInt(MAX_COLUMNA_T);//ira del 0-9 por que la matriz es de 10 valores
-
-                tablero[filaAleatorio][columnaAleatorio] = 'B';//ponemos a bart dentro de una posicion aleatoria
-
-       printtablero();//llamamos a la impresion del tablero para que me muestre como se esta viendo
-
-        //PARTE DE 10 HOMERO
+        int filaAleatorio;
+        int columnaAleatorio;
+        for (int i = 0; i < numRepeticiones; i++) {
+        do {
+            filaAleatorio = aleatorio.nextInt(MAX_FILA_T);//ira del 0-9 por que la matriz es de 10 valores
+            columnaAleatorio = aleatorio.nextInt(MAX_COLUMNA_T);//ira del 0-9 por que la matriz es de 10 valores
+        }while (tablero[filaAleatorio][columnaAleatorio] !='L');
+        tablero[filaAleatorio][columnaAleatorio] = caracter;//ponemos a bart dentro de una posicion aleatoria
+    }
+    }
+    private static void AsignarHomeroACasillasLibres(char caracter, int numRepeticiones) {
+        Random aleatorio = new Random();
         int FilaAleatorioHomer;//definimos las variables q usaremos
         int ColumnaAleatorioHomer;
-            for (int i = 0; i < 10; i++) {//hacemos que se repita 10 veces
-                do {//usamos un DO para este bucle para q si o si se imprima
-                    FilaAleatorioHomer = aleatorio.nextInt(MAX_FILA_T);//seleccionamos un numero de fila aleatorio
-                    ColumnaAleatorioHomer = aleatorio.nextInt(MAX_COLUMNA_T);//seleccionamos un numero de columna aleatorio
-                }while (tablero[FilaAleatorioHomer][ColumnaAleatorioHomer] != 'L');
-                //ponemos la condicion de que si la casilla contiene algo distinto de L no ponga nada dentro de esta
+        for (int i = 0; i < numRepeticiones; i++) {//hacemos que se repita 10 veces
+            do {//usamos un DO para este bucle para q si o si se imprima
+                FilaAleatorioHomer = aleatorio.nextInt(MAX_FILA_T);//seleccionamos un numero de fila aleatorio
+                ColumnaAleatorioHomer = aleatorio.nextInt(MAX_COLUMNA_T);//seleccionamos un numero de columna aleatorio
+            }while (tablero[FilaAleatorioHomer][ColumnaAleatorioHomer] != 'L');
+            //ponemos la condicion de que si la casilla contiene algo distinto de L no ponga nada dentro de esta
+            tablero[FilaAleatorioHomer][ColumnaAleatorioHomer] = caracter;//llenamos la coordenada con 'H'
+        }
+    }
+    private static void AsiganarMuroAcasillasLibres(char caracter, int numRepeticiones) {
+        Random aleatorio = new Random();
+        int FilaAleatorioMuro;//definimos las variables que usaremos
+        int ColumnaAleatorioMuro;
+        for (int i = 0; i < numRepeticiones; i++) {//hacemos que se repita 10 veces
+            do {//usamos un DO para este bucle para q si o si se imprima
+                FilaAleatorioMuro = aleatorio.nextInt(MAX_FILA_T);//seleccionamos un numero de fila aleatorio
+                ColumnaAleatorioMuro = aleatorio.nextInt(MAX_COLUMNA_T);//seleccionamos un numero de columna aleatorio
+            }while (tablero[FilaAleatorioMuro][ColumnaAleatorioMuro] != 'L' );
+            //ponemos la condicion de que si la casilla contiene algo distinto de L no ponga nada dentro de esta
 
-                tablero[FilaAleatorioHomer][ColumnaAleatorioHomer] = 'H';//llenamos la coordenada con 'H'
-
-            }
+            tablero[FilaAleatorioMuro][ColumnaAleatorioMuro] = caracter;//llenamos de muros
+        }
+    }
+    public static void main(String[] args) {
+        tablero=new char[MAX_FILA_T][MAX_COLUMNA_T];//traemos el tablero dentro de este void
+                 //LLENAR TABLERO 'L'
+        AsignarCaracterACasillasLibres('L');
+                 //mostrar tablero arriba esta definido
+       printtablero();
+                //PARTE DE BART
+        AsignarBartACasillaLibre('B',1);
+        printtablero();//llamamos a la impresion del tablero para que me muestre como se esta viendo
+        //PARTE DE 10 HOMERO
+        AsignarHomeroACasillasLibres('H',10);
         printtablero();
-
-            //CREAR MUROS
-            int FilaAleatorioMuro;//definimos las variables q usaremos
-            int ColumnaAleatorioMuro;
-                for (int i = 0; i < 10; i++) {//hacemos que se repita 10 veces
-                    do {//usamos un DO para este bucle para q si o si se imprima
-                        FilaAleatorioMuro = aleatorio.nextInt(MAX_FILA_T);//seleccionamos un numero de fila aleatorio
-                        ColumnaAleatorioMuro = aleatorio.nextInt(MAX_COLUMNA_T);//seleccionamos un numero de columna aleatorio
-                    }while (tablero[FilaAleatorioMuro][ColumnaAleatorioMuro] != 'L' );
-                    //ponemos la condicion de que si la casilla contiene algo distinto de L no ponga nada dentro de esta
-
-                    tablero[FilaAleatorioMuro][ColumnaAleatorioMuro] = 'M';//llenamos de muros
-                }
+                      //CREAR MUROS
+        AsiganarMuroAcasillasLibres('M',10);
         printtablero();
             tablero[MAX_FILA_T - 1][MAX_COLUMNA_T - 1]= 'F';//ponemos la F en la posicion (9,9) para el final del juego
         printtablero();
