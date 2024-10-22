e540cef14adcdfb924ed6f5ff8b1746990679312
JimenezAndrewJ <andrewjairjh@gmail.com>
2024-10-22 19:22:09 +0200

    [22/10/2024 19:21 ordenar con funciones]
    [22/10/2024 19:21 ordenar con funciones]

---
+++ arreglar-codigo/src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    private static final int filas = 10;
+    private static final int columnas = 9;
+    private static int tablero[][] = new int[filas][columnas];
+
+    private static void llenarNumeros(){
+        Random random = new Random();
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                //int numAleatorio= random.nextInt(10)+10*(j+1); //10 - 19
+                int numAleatorio = random.nextInt(10 * (j + 1), (10 * (j + 1) + 10));
+                tablero[i][j] = numAleatorio;
+            }
+        }
+    }
+
+    private static void ordenarNumeros(){
+        for (int columna = 0; columna < 9; columna++) {
+
+            for (int i = 0; i < filas - 1; i++) {
+                for (int j = i; j < filas; j++) {
+                    if (tablero[i][columna] > tablero[j][columna]) {
+                        int aux = tablero[i][columna];
+                        tablero[i][columna] = tablero[j][columna];
+                        tablero[j][columna] = aux;
+                    }
+                }
+            }
+
+        }
+    }
+
+    private static void imprimirTabla(){
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(tablero[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        llenarNumeros();
+        ordenarNumeros();
+        imprimirTabla();
+             
+    }
+}
