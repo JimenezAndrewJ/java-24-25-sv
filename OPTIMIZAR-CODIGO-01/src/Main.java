1a2671be16aad84f3dbaa6e3a8c43b80b0a5555b
JimenezAndrewJ <andrewjairjh@gmail.com>
2024-10-22 19:09:13 +0200

    [22/10/2024 19:09 optimizar codigo 01]
    [22/10/2024 19:09 optimizar codigo 01]

---
+++ OPTIMIZAR-CODIGO-01/src/Main.java
@@ -0,0 +1,69 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+            public static void main(String[] args) {
+                String block = String.valueOf("CLAY");
+
+                switch (block) {
+                    case "IRON":
+                        System.out.println("hola");
+                        break;
+                    case "BED":
+                        break;
+                    case "BED_BLOCK":
+                        break;
+                    case "CACTUS":
+                        break;
+                    case "BRICK_STAIRS":
+                        break;
+                    case "BIRCH_WOOD_STAIRS":
+                        break;
+                    case "BREWING_STAND":
+                        break;
+                    case "CAKE":
+                        break;
+                    case "CAKE_BLOCK":
+                        break;
+                    case "CAULDRON":
+                        break;
+                    case "CHEST":
+                        break;
+                    case "CLAY":
+                        System.out.println("CLAY");
+                        break;
+                    case "SAND":
+                        break;
+                    case "BURNING_FURNACE":
+                        break;
+                    case "COBBLESTONE_STAIRS":
+                        break;
+                    case "FENCE":
+                        break;
+                    case "FENCE_GATE":
+                        break;
+                    case "FIRE":
+                        break;
+                    case "GRAVEL":
+                        break;
+                    case "IRON_DOOR_BLOCK":
+                        break;
+                    case "IRON_FENCE":
+                        break;
+                    case "IRON_DOOR":
+                        break;
+                    case "LADDER":
+                        break;
+                    case "MELON":
+                        break;
+                    case "LEAVES":
+                        break;
+                    case "LOCKED_CHEST":
+                        break;
+                    case "SANDSTONE_STAIRS":
+                        break;
+
+                }
+            }
+        }
+
