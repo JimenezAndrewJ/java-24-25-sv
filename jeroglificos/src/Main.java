a612777eb14a41a3cd86ca7b3db46f11e9b75004
JimenezAndrewJ <andrewjairjh@gmail.com>
2024-10-30 10:19:33 +0100

    Update Main.java

--- jeroglificos/src/Main.java
+++ jeroglificos/src/Main.java
@@ -7,29 +7,29 @@ public class Main {
 
         // Mapear las potencias de 10 con sus símbolos correspondientes
         char[] simbolos = {'H', 'R', 'D', 'F', 'C', 'G', 'T'}; // De 1,000,000 a 1
-        int[] valores = {1000000, 100000, 10000, 1000, 100, 10, 1};
+        int[] val = {1000000, 100000, 10000, 1000, 100, 10, 1};
 
         while (true) {
             // Leer el número de entrada
-            System.out.print("Dame un numero para convertir a jerogrifico: ");
-            int numero = scanner.nextInt();
+            System.out.print("Dame un numero y lo paso a jerogrifico: ");
+            int num = scanner.nextInt();
 
             // Si el número es 0, terminar el programa
-            if (numero == 0) break;
-            if (numero > 100000 || numero < 0) {
-                System.out.println("El numero es Invalido");
+            if (num == 0) break;
+            if (num > 100000 || num < 0) {
+                System.out.println("El numero no es valido");
                 break;
             }
 
             StringBuilder resultado = new StringBuilder();
 
             // Descomponer el número en cada potencia de 10
-            for (int i = 0; i < valores.length; i++) {
-                int cantidad = numero / valores[i]; // Cuántas veces cabe esta potencia en el número
-                numero %= valores[i];               // Reducir el número al resto
+            for (int i = 0; i < val.length; i++) {
+                int cant = num / val[i]; // Cuántas veces cabe esta potencia en el número
+                num %= val[i];               // dividimos el numero
 
-                // Agregar el símbolo correspondiente la cantidad de veces necesaria
-                for (int j = 0; j < cantidad; j++) {
+                // agregar el simbolo a los numeros correspondiestes
+                for (int j = 0; j < cant; j++) {
                     resultado.append(simbolos[i]);
                 }
             }
